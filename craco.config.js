bcca6632c2a3325ad075e76375b2e63cb3693bd2
yikina <1350227931@qq.com>
2023-05-21 21:34:44 +0800

    chore:完成项目打包优化

--- craco.config.js
+++ craco.config.js
@@ -1,8 +1,42 @@
-
+const TerserPlugin = require('terser-webpack-plugin');
+const CompressionPlugin = require('compression-webpack-plugin');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 module.exports = {
   // ...
   typescript: {
     enableTypeChecking: true /* (default value) */,
+  },
+  webpack: {
+    configure: (webpackConfig, { env, paths }) => {
+      // 使用TerserPlugin进行代码压缩
+      webpackConfig.optimization.minimizer = [
+        new TerserPlugin({
+          terserOptions: {
+            compress: {
+              drop_console: true, // 移除console.log语句
+            },
+          },
+        }),
+      ];
+
+      // 使用CompressionPlugin进行gzip压缩
+      webpackConfig.plugins.push(
+        new CompressionPlugin({
+          test: /\.(js|css)$/,
+          filename: '[path].gz[query]',
+          algorithm: 'gzip',
+          threshold: 10240, // 10KB以上的文件进行压缩
+          minRatio: 0.8,
+        })
+      );
+
+      // 使用BundleAnalyzerPlugin分析打包后的模块大小
+      if (env === 'production') {
+        webpackConfig.plugins.push(new BundleAnalyzerPlugin());
+      }
+
+      return webpackConfig;
+    },
   },
 };
